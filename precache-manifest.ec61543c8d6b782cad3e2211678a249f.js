1e00d2e0e19556d62bddf832211d75af7fb21284
jordanduran <46588887+jordanduran@users.noreply.github.com>
2019-10-07 21:57:22 -0400

    Update precache-manifest.ec61543c8d6b782cad3e2211678a249f.js

--- precache-manifest.ec61543c8d6b782cad3e2211678a249f.js
+++ precache-manifest.ec61543c8d6b782cad3e2211678a249f.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "ca72542d68247e88ec8a0f26d4605a1d",
-    "url": "/index.html"
+    "url": "/Noteful/index.html"
   },
   {
     "revision": "a7e066818eb8e7d2de1a",
-    "url": "/static/css/main.ac579737.chunk.css"
+    "url": "/Noteful/static/css/main.ac579737.chunk.css"
   },
   {
     "revision": "013e7783d6534e37cd2b",
-    "url": "/static/js/2.a6311b96.chunk.js"
+    "url": "/Noteful/static/js/2.a6311b96.chunk.js"
   },
   {
     "revision": "a7e066818eb8e7d2de1a",
-    "url": "/static/js/main.1a724463.chunk.js"
+    "url": "/Noteful/static/js/main.1a724463.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "url": "/Noteful/static/js/runtime~main.a8a9905a.js"
   }
 ]);
