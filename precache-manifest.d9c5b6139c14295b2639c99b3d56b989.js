fb535234049459b16f39525a8ee095cc789997ad
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2019-10-07 21:09:20 -0400

    Updates

---
+++ precache-manifest.d9c5b6139c14295b2639c99b3d56b989.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12777a483ed1fba4b5934820acd2f0c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67ccc74665d0f204441b",
+    "url": "/static/css/main.ac579737.chunk.css"
+  },
+  {
+    "revision": "013e7783d6534e37cd2b",
+    "url": "/static/js/2.a6311b96.chunk.js"
+  },
+  {
+    "revision": "67ccc74665d0f204441b",
+    "url": "/static/js/main.b7c9f6fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
